1195685eafd5459cba2c8fc99dfb28c8a408b7e4
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-27 19:29:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,7 @@
 class Dog
   
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end
-  
-  def name=(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-    
   end
 end
